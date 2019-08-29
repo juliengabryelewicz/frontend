0f2eb8bdebe1a7d86d5a79fe5ec8801c861d0edd
Julien <jgabryelewicz@adexos.fr>
2019-08-30 00:14:43 +0200

    change links

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -4,21 +4,21 @@ const renderSitemap = (posts,pages,categories,generals) => `<?xml version="1.0" 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 ${posts.map(post => `
   <url>
-    <loc>https://juliengabryelewicz.fr/${post.slug}</loc>
+    <loc>https://juliengabryelewicz.fr/blog/post/${post.slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
 `).join('\n')}
 ${pages.map(page => `
   <url>
-    <loc>https://juliengabryelewicz.fr/${page.slug}</loc>
+    <loc>https://juliengabryelewicz.fr/page/${page.slug}</loc>
     <changefreq>monthly</changefreq>
     <priority>0.5</priority>
   </url>
 `).join('\n')}
 ${categories.map(category => `
   <url>
-    <loc>https://juliengabryelewicz.fr/${category.slug}</loc>
+    <loc>https://juliengabryelewicz.fr/blog/category/${category.slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>
